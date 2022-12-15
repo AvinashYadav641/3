8cba59866bba023b8f3f58b5e66fcc338d4548d6
Avinah Yadav <94887332+AvinashYadav641@users.noreply.github.com>
2022-12-16 00:39:38 +0530

    Uploads

---
+++ terminitated_whatsapp/helper.py
@@ -0,0 +1,48 @@
+from urlextract import URLExtract
+from wordcloud import Wordcloud
+import matplotlib.pyplot as plt
+extract = URLExtract()
+
+def fetch_stats(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch the number of messages
+    num_messages = df.shape[0]
+
+    # fetch the total number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch number of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words), num_media_messages, len(links)
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+
+    return x, df
+    # name = x.index
+    # count = x.values
+    # plt.bar(name, count)
+    # plt.xticks(rotation='vertical')
+    # plt.show()
+
+
+def create_wordcloud(selected_user, df):
+
+    if selected_user != 'Overall':
+        df= df[df['user'] == selected_user]
+
+    wc =Wordcloud(width=500, height=500, min_font_size=10,background_color='white')
+    df_wc = wc.gentrate(df['message'].str.cat(sep=" "))
+    return df_wc
